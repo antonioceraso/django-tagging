117d15d81743db639e759a39b275a1a255c2dcd6
onur mat <omat@gezgin.com>
2010-05-26 20:39:19 +0300

    updated related tag doc tests

--- tagging/tests.py
+++ tagging/tests.py
@@ -1,32 +1,21 @@
 __test__ = {"doctest": """
 
 Related tag tests
->>> from tags.models import Tag, RelatedTag
->>> t1 = Tag.objects.create(name='t1')
->>> t2 = Tag.objects.create(name='t2')
->>> t3 = Tag.objects.create(name='t3')
->>> t4 = Tag.objects.create(name='t4')
->>> RelatedTag.objects.relate([t1, t2, t3])
->>> t1.get_related()
-[<Tag: t2>, <Tag: t3>]
->>> t2.get_related()
-[<Tag: t1>, <Tag: t3>]
->>> t3.get_related()
-[<Tag: t1>, <Tag: t2>]
->>> related_tag = RelatedTag.objects.get(tag=t1, related_tag=t2)
->>> related_tag.related = False
->>> related_tag.save()
->>> symm = RelatedTag.objects.get(tag=t2, related_tag=t1)
->>> symm.related
-False
->>> t1.get_related()
-[<Tag: t3>]
-# testing relate with deep=True
->>> RelatedTag.objects.relate([t1, t4], depth=1)
->>> t4.get_related()
-[<Tag: t1>, <Tag: t3>]
->>> t3.get_related()
-[<Tag: t1>, <Tag: t2>, <Tag: t4>]
-
+>>> from tagging.models import *
+>>> from tagging.utils import *
+>>> a=Tag.objects.create(name='a')
+>>> b=Tag.objects.create(name='b')
+>>> RelatedTag.objects.relate_all('a, b')
+>>> RelatedTag.objects.all()
+[<RelatedTag: a ~ b>, <RelatedTag: b ~ a>]
+>>> RelatedTag.objects.relate('a', '>', 'b')
+>>> RelatedTag.objects.all()
+[<RelatedTag: a > b>, <RelatedTag: b < a>]
+>>> RelatedTag.objects.relate('a', '.', 'b')
+>>> RelatedTag.objects.all()
+[<RelatedTag: a . b>, <RelatedTag: b _ a>]
+>>> RelatedTag.objects.relate('a', '~', 'b')
+>>> RelatedTag.objects.all()
+[<RelatedTag: a ~ b>, <RelatedTag: b ~ a>]
 """}
 
