38cffe07108594d47b2cef19932df552994ac96d
onur mat <omat@gezgin.com>
2010-05-26 17:29:17 +0300

    added a test project for testing the standalone tagging application via shell

---
+++ test_project/settings.py
@@ -0,0 +1,16 @@
+# minimal settings for testing tagging via shell
+
+import os, sys
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "../"))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'test.db'
+
+INSTALLED_APPS = (
+    'tagging',
+)
