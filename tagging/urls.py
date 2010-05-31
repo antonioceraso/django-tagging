bee44bd9fed08560a4d83af1459ca2014ef32682
onur mat <omat@gezgin.com>
2010-05-31 19:57:05 +0300

    minor fix

--- tagging/urls.py
+++ tagging/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('tagging.views',
     url(r'^search/$', 'tag_search', name='tag_search'),
-    url(r'^search/ac/$', 'tag_search_autocomplete', name='tag_search_autocomplete'),
+    url(r'^search/ac/$', 'search_autocomplete', name='tag_search_autocomplete'),
 )
