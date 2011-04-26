998a0446621699add9e43e5158ec9cd64866245d
William Grzybowski <william@agencialivre.com.br>
2011-04-26 20:43:58 -0300

    a few changes to support basic django 1.3

--- tagging/urls.py
+++ tagging/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls.defaults import *
-
+from django.conf.urls.defaults import patterns, url
+from views import TaggedItemListView
 
 urlpatterns = patterns('tagging.views',
     url(r'^ac/$', 'search_autocomplete', name='tag_search_ac'),
     url(r'^ac/(?P<category_slug>[-\w]+)/$', 'search_autocomplete', name='tag_search_ac_category'),
-    url(r'^(?P<tag_slug>[-+\w]+)/$', 'taggeditem_list', name='tag_detail'),
-    url(r'^(?P<app_name>([\w]+))/(?P<tag_slug>[-+\w]+)/$', 'taggeditem_list', name='tag_app_list'),
+    url(r'^(?P<tag_slug>[-+\w]+)/$', TaggedItemListView.as_view(), name='tag_detail'),
+    url(r'^(?P<app_name>([\w]+))/(?P<tag_slug>[-+\w]+)/$', TaggedItemListView.as_view(), name='tag_app_list'),
 )
